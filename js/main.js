6b78e09e49843242dd85fba63dd62c2ea780ec42
Vinicius Martins <vinicius.martins.vsm@outlook.com>
2023-04-23 21:32:32 -0300

    Adding the logical JS to encrypt the text

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 let encryptorArea = document.querySelector('[data-encryptor-area]');
 let decryptor = document.querySelector('[data-decryptor]');
-let decryptorTitle = document.querySelector('[data-decryptor-title]');
-let decryptorDescription = document.querySelector('[data-decryptor-description]');
+let btnEncrypt = document.querySelector('[data-btn-encrypt]');
+let btnDecrypt = document.querySelector('[data-btn-decrypt]');
 
-encryptorArea.addEventListener("keyup", (evento) => {
-    decryptor.innerHTML = evento.target.value;
+
+btnEncrypt.addEventListener('click', () => {
+    const encryptText = encrypt(encryptorArea.value);
+    decryptor.textContent = encryptText;
+    encryptorArea.value = "";
 })
+
+function encrypt(encryptString) {
+    let matrixCode = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    encryptString = encryptString.toLowerCase();
+
+    for (code in matrixCode) {
+        if (encryptString.includes(matrixCode[code][0])) {
+            encryptString = encryptString.replaceAll(matrixCode[code][0], matrixCode[code][1]);
+        }
+    }
+
+    return encryptString;
+}
